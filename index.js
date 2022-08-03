8ae8e5c274b9bdb2f24af794a853ea81c19ce543
Dorcus-1 <97876733+Dorcus-1@users.noreply.github.com>
2022-08-03 12:11:35 +0200

    some chsnge on responsiveness

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var myName= "my name is Kevin";
+var myAge = 19;
+
+// console.log(myName);
+// console.log(myAge);
+
+var num1=3
+var num2=4
+var sum = num1 + num2;
+console.log(`the sum is ${sum}`);
